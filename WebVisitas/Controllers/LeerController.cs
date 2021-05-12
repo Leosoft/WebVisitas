4cf4ca7552a451fcee6a5b57e3f6f6a899160683
Leonardo Zabala <leosoft3031@gmail.com>
2021-05-12 10:59:10 -0300

    Agregar archivos de proyecto.

---
+++ WebVisitas/Controllers/LeerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebVisitas.Controllers
+{
+    public class LeerController : Controller
+    {
+        private readonly IWebHostEnvironment _WebHostEnvironment;
+
+
+        public LeerController(IWebHostEnvironment webHostEnvironment)
+        {
+            _WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            string webRootPath = _WebHostEnvironment.WebRootPath;
+            string data;
+
+            try
+            {
+                string path = Path.Combine(webRootPath, "archivo.txt");
+                data = System.IO.File.ReadAllText(path, Encoding.UTF8);
+            }
+            catch (Exception e)
+            {
+                data = "Exception: " + e.Message;
+            }
+
+
+            data = data.Replace("\n", Environment.NewLine);
+            return View(null, data);
+        }
+    }
+}
---
+++ WebVisitas/Controllers/VisitaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebVisitas.Controllers
+{
+    public class VisitaController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+
+        public VisitaController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        public IActionResult Index()
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+
+            try
+            {
+                string observaciones = HttpContext.Request.Form["FirstName"] + "\nComentario: " + HttpContext.Request.Form["observaciones"];
+                string separar = "\n___________________\n";
+                string fecha = System.DateTime.Now.ToString("yyyyMMdd");
+                string hora = System.DateTime.Now.ToString("HH:mm:ss");
+
+                System.IO.StreamWriter archivo = new StreamWriter(Path.Combine(webRootPath, "archivo.txt"), true);
+                archivo.WriteLine("\nFecha: " + fecha + "\nHora " + hora + "\nAutor: " + observaciones + separar);
+                archivo.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            return View();
+        }
+    }
+}
